bc6c7a65994b837a110b9207c9fc2a48e92bc844
Meesterdaaf <d.vanoudheusden@rocva.nl>
2021-11-24 14:39:03 +0100

    added uniqueness and moved class

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -3,9 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-
-use Database\Factories\TagEnum;
-
+use Database\Factories\Enums\TagEnum;
 use Spatie\Enum\Faker\FakerEnumProvider;
 
 class TagFactory extends Factory
@@ -22,9 +20,11 @@ class TagFactory extends Factory
     {
         $this->faker->addProvider(new FakerEnumProvider($this->faker));
 
+        $randomTag = $this->faker->unique()->randomEnumValue(TagEnum::class);
+
         return
             [
-                'name' => $this->faker->randomEnum(TagEnum::class)
+                'name' => $randomTag
             ];
     }
 }
